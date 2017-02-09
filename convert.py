bd50438bb752f36bd75caa6c221b75f2062a4dfc
firelighted <asano.sar@gmail.com>
2017-02-08 23:21:13 -0500

    phase1_sasano
    Initial file setup

---
+++ convert.py
@@ -0,0 +1,4 @@
+import cc_dat_utils
+make
+# loading DAT file
+# print data
---
+++ main.py
@@ -0,0 +1,2 @@
+import cc_dat_utils
+make_cc_data_from_dat('C:\Users\Sarah\Documents\GitHub\cc_tools\data\pfgd_test.dat')
