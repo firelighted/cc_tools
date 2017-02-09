155aead98f519799cbf544d9f8081ff463f58eef
firelighted <asano.sar@gmail.com>
2017-02-09 09:18:25 -0500

    Complete convert.py, pfgd_test.txt file generated
    Complete convert.py (import provided code, read in dat file, write dat
    file data to pfgd_test.dat), leave main open for other operations

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-import cc_dat_utils
-make_cc_data_from_dat('C:\Users\Sarah\Documents\GitHub\cc_tools\data\pfgd_test.dat')
+from cc_dat_utils import make_cc_data_from_dat
+
+thatdata = make_cc_data_from_dat('data\pfgd_test.dat')
+print(str(thatdata))
+
+with open('pfgd_test.txt', 'w') as f:
+    f.write(str(thatdata))
+f.close()
